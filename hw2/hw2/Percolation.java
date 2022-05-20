95b914547433064bb3cafc58e50bb8e3bc6afb09
dxw1997 <1325596956@qq.com>
2022-05-20 00:32:26 -0700

    finish hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,81 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * Using Union-find method.
+ * Adding two virtual nodes, one is on the top and one is at the bottom.
+ */
 public class Percolation {
+    private int N;
+    private boolean[][] pMatrix;
+    private WeightedQuickUnionUF wq;
+    private int openSitesNum;
 
+    /**
+     * Converting a position(x,y) in a matrix to an interger
+     * @param r row number
+     * @param c column number
+     * @return the converted number
+     */
+    private int xyTo1D(int r, int c){
+        if(r == -1) return 0;
+        else if(r == N) return N*N+1;
+        return r*N+c+1;
+    }
+
+    /**
+     * Checking whether a position is out of bound.
+     * @param r row number
+     * @param c column number
+     */
+    private void check(int r, int c){
+        if(r < 0 || r >= N || c < 0 || c >= N) throw new IndexOutOfBoundsException();
+    }
+
+    public Percolation(int N){
+        if(N <= 0) throw new IllegalArgumentException();
+        this.N = N;
+        pMatrix = new boolean[N][N];
+        wq = new WeightedQuickUnionUF(N*N+2);
+        openSitesNum = 0;
+    }
+    public void open(int row, int col){
+        check(row, col);
+        if(!pMatrix[row][col]){
+            ++openSitesNum;
+            pMatrix[row][col] = true;
+            if(row == 0 || isOpen(row-1, col)) wq.union(xyTo1D(row, col), xyTo1D(row-1, col));
+            if(row == N-1 || isOpen(row+1, col)) wq.union(xyTo1D(row, col), xyTo1D(row+1, col));
+            if(col > 0 && isOpen(row, col-1)) wq.union(xyTo1D(row, col), xyTo1D(row, col-1));
+            if(col < N-1 && isOpen(row, col+1)) wq.union(xyTo1D(row, col), xyTo1D(row, col+1));
+        }
+    }
+    public boolean isOpen(int row, int col){
+        check(row, col);
+        return pMatrix[row][col];
+    }
+    public boolean isFull(int row, int col){
+        check(row, col);
+        return wq.connected(xyTo1D(row, col), xyTo1D(-1, col));
+    }
+    public int numberOfOpenSites(){
+        return openSitesNum;
+    }
+    public boolean percolates(){
+        return wq.connected(xyTo1D(-1, 0), xyTo1D(N, 0));
+    }
+
+    public static void main(String[] args){
+        ///for testing
+        Percolation p = new Percolation(3);
+        p.open(0,0);
+        p.open(1,1);
+        if(p.numberOfOpenSites() != 2) System.out.print("wrong sites number.\n");
+        if(p.isFull(1,1)) System.out.print("wrong is Full\n");
+        p.open(1,0);
+        if(!p.isFull(1,1)) System.out.print("wrong is Full\n");
+        p.open(2,1);
+        if(!p.percolates()) System.out.print("wrong percolate\n");
+        if(p.numberOfOpenSites() != 4) System.out.print("wrong sites number.\n");
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,56 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int N;
+    private int T;
+    private PercolationFactory pf;
+    private double mean;
+    private double stddev;
+
+    private void compute(){
+        double[] x = new double[T];
+        for(int i = 0;i < T;++i ){
+            Percolation p = pf.make(N);
+            while(!p.percolates()){
+                int r = StdRandom.uniform(0,N);
+                int c = StdRandom.uniform(0, N);
+                if(p.isOpen(r,c)) continue;
+                p.open(r,c);
+            }
+            x[i] = 1.0*p.numberOfOpenSites()/(N*N);
+        }
+//        for(int i = 0;i < T;++i ){
+//            mean += x[i];
+//        }
+//        mean /= T;
+        mean = StdStats.mean(x);
+//        for(int i = 0;i < T;++i ){
+//            stddev += (x[i]-mean)*(x[i]-mean);
+//        }
+//        stddev /= (T-1);
+        stddev = StdStats.stddev(x);
+    }
+
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if(N <= 0 || T <=0 || pf==null) throw new IllegalArgumentException();
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        compute();
+    }
+    public double mean(){
+        return mean;
+    }
+    public double stddev(){
+        return stddev;
+    }
+    public double confidenceLow(){
+        return mean - 1.96*stddev/Math.sqrt(T);
+    }
+    public double confidenceHigh(){
+        return mean + 1.96*stddev/Math.sqrt(T);
+    }
 }
